53cf4b51c0dedb62dbdc58ba736a8201a74ba48a
Ahmed Essam <146502595+AhmedEssam2209@users.noreply.github.com>
2024-03-06 22:49:13 +0200

    Add files via upload

---
+++ Migrations/20230928035122_addedINTtime.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieRateApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedINTtime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ation",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ation",
+                table: "Movies");
+        }
+    }
+}
---
+++ Migrations/20230929010732_removedDuration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieRateApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class removedDuration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Movies");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Duration",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
